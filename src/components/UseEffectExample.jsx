a86e0da791536aee26e0d4e93eb7ebdf737a5c1a
Stefano Casasola <ste.casasola@gmail.com>
2022-11-29 11:16:40 +0100

    Finishes the examples

--- src/components/UseEffectExample.jsx
+++ src/components/UseEffectExample.jsx
@@ -13,13 +13,33 @@ const UseEffectExample = () => {
   // - componentWillUnmount (which we didn't cover)
 
   const [counter, setCounter] = useState(10)
+  const [name, setName] = useState('Stefano')
 
-  useEffect(() => {
-    console.log('this component just mounted!')
-  }, [])
+  //   useEffect(() => {
+  //     console.log('the component got an update!')
+  //     // setCounter(counter + 1)
+  //   })
   // 1 million $$$ question: what is the dependencies Array?
   // the deps array is an array of values: whenever even ONE of those values
   // change, the callback will be called AGAIN
+  // known this, let's try to list down the most used signatures for useEffect:
+
+  // COMPONENTDIDMOUNT REPLACEMENT:
+  useEffect(() => {
+    console.log('this is a replacement for componentDidMount!')
+  }, [])
+
+  // GENERIC COMPONENTDIDUPDATE REPLACEMENT (not recommended):
+  useEffect(() => {
+    console.log(
+      'this is going to be called at every update, like componentDidUpdate!'
+    )
+  })
+
+  // COMPONENTDIDUPDATE THAT TRIGGERS JUST WHEN 1 STATE PROPERTY CHANGES:
+  useEffect(() => {
+    console.log('name changed!')
+  }, [name])
 
   return (
     <div>
@@ -27,6 +47,7 @@ const UseEffectExample = () => {
         useEffect example! Welcome :D
       </h3>
       <p>{counter}</p>
+      <p onClick={() => setName('Eric')}>{name}</p>
     </div>
   )
 }
